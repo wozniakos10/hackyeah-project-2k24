c964a2f5be3690e242a33880a5c18c3448921888
Mateusz Tylec <mateusztylec@duck.com>
2024-09-28 19:56:20 +0200

    update

--- src/hackyeah_project_lib/web/ui.py
+++ src/hackyeah_project_lib/web/ui.py
@@ -1,46 +1,46 @@
 import streamlit as st
-from openai import OpenAI
+from langfuse.openai import openai
 import os
+import uuid  # Import the uuid module
 from hackyeah_project_lib.config import settings
+from hackyeah_project_lib.transcription_srt import transcribe_audio_srt
+from hackyeah_project_lib.transcription import transcribe_audio
 
 OPENAI_API_KEY = settings.OPENAI_API_KEY
 
-st.title("💬 Chatbot with Audio Upload")
+result = openai.langfuse_auth_check()
+st.title("💬 chatuj tutaj")
 
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state["messages"] = [{"role": "assistant", "content": "Dodaj plik aby rozpocząć"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 # File Uploader for MP3 Files
-uploaded_file = st.file_uploader("Upload an MP3 file", type=["mp3"])
+uploaded_file = st.file_uploader("Upload file", type=["mp3", "mp4"])
 
 if uploaded_file is not None:
     # Ensure the 'temp' directory exists
     os.makedirs("temp", exist_ok=True)
     
-    # Save the uploaded MP3 file to a temporary location
-    file_path = os.path.join("temp", uploaded_file.name)
+    # Generate a unique filename using UUID
+    unique_id = uuid.uuid4().hex
+    filename, file_extension = os.path.splitext(uploaded_file.name)
+    unique_filename = f"{filename}_{unique_id}{file_extension}"
+    
+    # Save the uploaded file with the unique filename
+    file_path = os.path.join("temp", unique_filename)
     with open(file_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
     
     st.audio(file_path, format="audio/mp3")
-
-    # Initialize OpenAI client
-    client = OpenAI(api_key=OPENAI_API_KEY)
-
-    # Append user message
     st.session_state.messages.append({"role": "user", "content": "Audio file uploaded."})
     st.chat_message("user").write("Audio file uploaded.")
 
-    # Process the audio file with OpenAI's Whisper
-    try:
-        with open(file_path, "rb") as audio_file:
-            response = client.audio.transcriptions.create(file=audio_file, model="whisper-1")
-        msg = response.text
-        
-        st.session_state.messages.append({"role": "assistant", "content": msg})
-        st.chat_message("assistant").write(msg)
-    except Exception as e:
-        st.error(f"An error occurred during transcription: {e}")
+
+    transcription = transcribe_audio_srt(file_path) #transkrypcja audio
+    st.chat_message("assistant").write("TRANSCRIPTION SRT:" + transcription)
+
+    transcription = transcribe_audio(file_path) #transkrypcja audio
+    st.chat_message("assistant").write("TRANSCRIPTION:" + transcription)
