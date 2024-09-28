6fa2ff926eaadb693d8f14b5282fb95d25af0e35
Mateusz Tylec <mateusztylec@duck.com>
2024-09-28 14:05:41 +0200

    update

--- src/hackyeah_project_lib/openai.py
+++ src/hackyeah_project_lib/openai.py
@@ -1,7 +1,6 @@
 from openai import OpenAI
-from hackyeah_project_lib.config import settings
 
-client = OpenAI(settings.OPENAI_API_KEY)
+client = OpenAI("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")
 
 audio_file= open("/Users/mateu/development/projects/hackyeah-project-2k24/src/stuff/speech.mp3", "rb")
 transcription = client.audio.transcriptions.create(
